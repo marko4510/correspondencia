f822719ce980f23755194b93ead9f481597de16a
jhowillneytan <jhowillneytan@gmail.com>
2024-08-14 18:00:45 -0400

    crud doc

--- src/main/java/com/example/Proyecto/Controller/DocumentoController.java
+++ src/main/java/com/example/Proyecto/Controller/DocumentoController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,6 +45,12 @@ public class DocumentoController {
     @PostMapping("/formulario")
     public String formulario(Model model) {
         model.addAttribute("documento", new Documento());
+        return "documento/formulario";
+    }
+
+    @PostMapping("/formulario/{id}")
+    public String formulario(Model model, @PathVariable("id")Long id) {
+        model.addAttribute("documento", documentoService.findById(id));
         return "documento/formulario";
     }
 
