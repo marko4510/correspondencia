f67b13325e751ba2a17a5753db31224c416c4487
jhowillneytan <jhowillneytan@gmail.com>
2024-09-02 09:05:41 -0400

    impresion de hoja ruta

--- src/main/java/com/example/Proyecto/Controller/SeguimientoController.java
+++ src/main/java/com/example/Proyecto/Controller/SeguimientoController.java
@@ -47,12 +47,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
-
-
 @Controller
 @RequestMapping(value = "/seguimiento")
 public class SeguimientoController {
-    
+
     @Autowired
     private MovimientoDocumentoService movimientoDocumentoService;
 
@@ -86,7 +84,7 @@ public class SeguimientoController {
                     .collect(Collectors.toSet());
 
             // Verifica el contenido de years
-    
+
             model.addAttribute("unidades", unidadService.findAll());
             model.addAttribute("years", years);
             model.addAttribute("opcion", "administrar seguimiento");
@@ -95,146 +93,196 @@ public class SeguimientoController {
             return "redirect:/login";
         }
     }
-    
 
-   @PostMapping("/buscar_documento/{nroRuta}/{year}/{id_unidad}")
-        public String buscar_documento(@PathVariable(name = "nroRuta") String nroRuta,
-        @PathVariable(name = "year")String year,
-        @PathVariable(name = "id_unidad")Integer id_unidad, Model model) {
+    @PostMapping("/buscar_documento/{nroRuta}/{year}/{id_unidad}")
+    public String buscar_documento(@PathVariable(name = "nroRuta") String nroRuta,
+            @PathVariable(name = "year") String year,
+            @PathVariable(name = "id_unidad") Integer id_unidad, Model model) {
 
-            // List<MovimientoDocumento> flujoDocumentos = movimientoDocumentoService.obtener_Flujo_Documento(nroRuta);
-            Integer num_ruta = Integer.parseInt(nroRuta);
-            List<MovimientoDocumento> flujoDocumentos = movimientoDocumentoService.obtener_Flujos_Documentos(num_ruta,id_unidad,year);
-            
-            if (flujoDocumentos.size() > 0) {
-                HojaRuta hojaRuta = flujoDocumentos.get(0).getHojaRuta();
-                model.addAttribute("flujo", flujoDocumentos);
-                model.addAttribute("hojaDeRuta", hojaRuta);
+        // List<MovimientoDocumento> flujoDocumentos =
+        // movimientoDocumentoService.obtener_Flujo_Documento(nroRuta);
+        Integer num_ruta = Integer.parseInt(nroRuta);
+        List<MovimientoDocumento> flujoDocumentos = movimientoDocumentoService.obtener_Flujos_Documentos(num_ruta,
+                id_unidad, year);
 
-                if (hojaRuta.getTipo_derivacion().equals("Interno")) {
+        if (flujoDocumentos.size() > 0) {
+            HojaRuta hojaRuta = flujoDocumentos.get(0).getHojaRuta();
+            model.addAttribute("flujo", flujoDocumentos);
+            model.addAttribute("hojaDeRuta", hojaRuta);
+
+            if (hojaRuta.getTipo_derivacion().equals("Interno")) {
                 Integer id_usuario_emisor = flujoDocumentos.get(0).getHojaRuta().getUsuario_emisor();
                 Long id_usuario_emisor_long = id_usuario_emisor.longValue();
-                Usuario usuario_emisor = usuarioService.findById(id_usuario_emisor_long); 
+                Usuario usuario_emisor = usuarioService.findById(id_usuario_emisor_long);
                 model.addAttribute("usuario_emisor", usuario_emisor);
                 model.addAttribute("interno", true);
-                }
-                if (hojaRuta.getTipo_derivacion().equals("Externo")) {
-                    model.addAttribute("usuario_emisor", hojaRuta.getEntidadExterna().getNombre());
-                    model.addAttribute("externo", true);
-                }
-               
-                return "seguimiento/flujoDocumento"; // Retorna la vista si hay resultados
-            } else {
-                return "seguimiento/noEncontrado"; // Retorna una vista alternativa si no hay resultados
             }
-        }
-
-        @GetMapping("/verDocumentoMovimiento/{id}")
-        public ResponseEntity<Resource> verDocumentoMovimiento(@PathVariable("id") Long id) {
-            MovimientoDocumento movimientoDocumento = movimientoDocumentoService.findById(id);
-
-            // Verifica si el movimientoDocumento no es nulo y si la ruta del movimiento no
-            // está vacía o es nula
-            if (movimientoDocumento != null && movimientoDocumento.getRuta_movimiento() != null
-                    && !movimientoDocumento.getRuta_movimiento().trim().isEmpty()) {
-                // Obtener la ruta completa del archivo
-                Path projectPath = Paths.get("").toAbsolutePath();
-                String ruta = projectPath.resolve("uploads").resolve(movimientoDocumento.getRuta_movimiento())
-                        .toString();
-                System.out.println("Ruta del archivo: " + ruta);
-
-                try {
-                    // Verificar si el archivo realmente existe en el sistema de archivos
-                    File file = new File(ruta);
-                    if (file.exists()) {
-                        // Cargar el archivo PDF como recurso
-                        Resource resource = new InputStreamResource(new FileInputStream(file));
-
-                        // Configurar las cabeceras de la respuesta
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.add(HttpHeaders.CONTENT_DISPOSITION,
-                                "inline; filename=" + movimientoDocumento.getHojaRuta().getId_hoja_ruta());
-                        headers.setContentType(MediaType.APPLICATION_PDF);
-
-                        // Devolver la respuesta con el archivo PDF
-                        return ResponseEntity.ok()
-                                .headers(headers)
-                                .body(resource);
-                    } else {
-                        // Archivo no encontrado en la ruta especificada
-                        return ResponseEntity.notFound().build();
-                    }
-                } catch (IOException e) {
-                    // Manejar excepciones de IO
-                    e.printStackTrace();
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-                }
-            } else {
-                // Devolver una respuesta 404 Not Found si el documento no existe o la ruta está
-                // vacía
-                return ResponseEntity.notFound().build();
+            if (hojaRuta.getTipo_derivacion().equals("Externo")) {
+                model.addAttribute("usuario_emisor", hojaRuta.getEntidadExterna().getNombre());
+                model.addAttribute("externo", true);
             }
+
+            return "seguimiento/flujoDocumento"; // Retorna la vista si hay resultados
+        } else {
+            return "seguimiento/noEncontrado"; // Retorna una vista alternativa si no hay resultados
         }
-    
-        @GetMapping("/hojaruta/{id_hoja_ruta}")
-        public ResponseEntity<?> generarHojaRuta(@PathVariable(name = "id_hoja_ruta") Long id_hoja_ruta, Model model,
-                HttpServletRequest request, HttpServletResponse response) {
+    }
 
-            Documento documento = documentoService.findById(id_hoja_ruta);
-            HojaRuta hojaRuta = hojaRutaService.findById(id_hoja_ruta);
-            String fechaCreacionStr = hojaRuta.getFechaCreacion().toString(); // Supongamos que es una cadena como
-                                                                               // '2024-08-21 16:55:29.393'
+    @GetMapping("/verDocumentoMovimiento/{id}")
+    public ResponseEntity<Resource> verDocumentoMovimiento(@PathVariable("id") Long id) {
+        MovimientoDocumento movimientoDocumento = movimientoDocumentoService.findById(id);
 
-            // Usa LocalDateTime y ajusta el formato del DateTimeFormatter
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-            LocalDateTime fechaCreacion = LocalDateTime.parse(fechaCreacionStr, formatter);
+        // Verifica si el movimientoDocumento no es nulo y si la ruta del movimiento no
+        // está vacía o es nula
+        if (movimientoDocumento != null && movimientoDocumento.getRuta_movimiento() != null
+                && !movimientoDocumento.getRuta_movimiento().trim().isEmpty()) {
+            // Obtener la ruta completa del archivo
+            Path projectPath = Paths.get("").toAbsolutePath();
+            String ruta = projectPath.resolve("uploads").resolve(movimientoDocumento.getRuta_movimiento())
+                    .toString();
+            System.out.println("Ruta del archivo: " + ruta);
 
-             // Formatea la fecha y hora al formato deseado
-            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm ");
-            String fechaCreacionFormatted = fechaCreacion.format(outputFormatter);
+            try {
+                // Verificar si el archivo realmente existe en el sistema de archivos
+                File file = new File(ruta);
+                if (file.exists()) {
+                    // Cargar el archivo PDF como recurso
+                    Resource resource = new InputStreamResource(new FileInputStream(file));
 
-            int year = fechaCreacion.getYear();
+                    // Configurar las cabeceras de la respuesta
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.add(HttpHeaders.CONTENT_DISPOSITION,
+                            "inline; filename=" + movimientoDocumento.getHojaRuta().getId_hoja_ruta());
+                    headers.setContentType(MediaType.APPLICATION_PDF);
 
-            model.addAttribute("hojaRuta", hojaRuta);
-            if (hojaRuta.getTipo_derivacion().equals("Interno")) {
-                Integer id_usuario_emisor = hojaRuta.getUsuario_emisor();
-                Long id_usuario_emisor_long = id_usuario_emisor.longValue();
-                Usuario usuario_emisor = usuarioService.findById(id_usuario_emisor_long); 
-                model.addAttribute("usuario_emisor", usuario_emisor);
-                model.addAttribute("interno", true);
+                    // Devolver la respuesta con el archivo PDF
+                    return ResponseEntity.ok()
+                            .headers(headers)
+                            .body(resource);
+                } else {
+                    // Archivo no encontrado en la ruta especificada
+                    return ResponseEntity.notFound().build();
                 }
-                if (hojaRuta.getTipo_derivacion().equals("Externo")) {
-                    model.addAttribute("usuario_emisor", hojaRuta.getEntidadExterna().getNombre());
-                    model.addAttribute("externo", true);
-                }
-
-            String textoQR = "Hoja de Ruta: " + hojaRuta.getNroRuta() + "/" + year + "\n" +
-                    "Fecha: " + fechaCreacionFormatted + "\n" +
-                    "Identificador: " + generarAlfanumerico(hojaRuta.getId_hoja_ruta());
-            model.addAttribute("textoQR", textoQR);
-
-            WebContext context = new WebContext(request, response, request.getServletContext());
-            context.setVariables(model.asMap());
-
-            String htmlContent = templateEngine.process("seguimiento/hojaDeRuta", context);
-
-            return ResponseEntity.ok().body(htmlContent);
-        }
-        
-        
-        public static String generarAlfanumerico(Long valor) {
-            String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            StringBuilder resultado = new StringBuilder();
-            Random random = new Random(valor); // Usamos el Long como semilla para el generador de números aleatorios
-    
-            // Determinamos la longitud de la cadena basándonos en el valor de Long (por ejemplo, 8 caracteres)
-            int longitud = 8;
-    
-            for (int i = 0; i < longitud; i++) {
-                int index = random.nextInt(caracteres.length());
-                resultado.append(caracteres.charAt(index));
+            } catch (IOException e) {
+                // Manejar excepciones de IO
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
             }
-    
-            return resultado.toString();
+        } else {
+            // Devolver una respuesta 404 Not Found si el documento no existe o la ruta está
+            // vacía
+            return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/hojaruta/{id_hoja_ruta}")
+    public ResponseEntity<?> generarHojaRuta(@PathVariable(name = "id_hoja_ruta") Long id_hoja_ruta, Model model,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        Documento documento = documentoService.findById(id_hoja_ruta);
+        HojaRuta hojaRuta = hojaRutaService.findById(id_hoja_ruta);
+        String fechaCreacionStr = hojaRuta.getFechaCreacion().toString(); // Supongamos que es una cadena como
+                                                                          // '2024-08-21 16:55:29.393'
+
+        // Usa LocalDateTime y ajusta el formato del DateTimeFormatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        LocalDateTime fechaCreacion = LocalDateTime.parse(fechaCreacionStr, formatter);
+
+        // Formatea la fecha y hora al formato deseado
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm ");
+        String fechaCreacionFormatted = fechaCreacion.format(outputFormatter);
+
+        int year = fechaCreacion.getYear();
+
+        model.addAttribute("hojaRuta", hojaRuta);
+        if (hojaRuta.getTipo_derivacion().equals("Interno")) {
+            Integer id_usuario_emisor = hojaRuta.getUsuario_emisor();
+            Long id_usuario_emisor_long = id_usuario_emisor.longValue();
+            Usuario usuario_emisor = usuarioService.findById(id_usuario_emisor_long);
+            model.addAttribute("usuario_emisor", usuario_emisor);
+            model.addAttribute("interno", true);
+        }
+        if (hojaRuta.getTipo_derivacion().equals("Externo")) {
+            model.addAttribute("usuario_emisor", hojaRuta.getEntidadExterna().getNombre());
+            model.addAttribute("externo", true);
+        }
+
+        String textoQR = "Hoja de Ruta: " + hojaRuta.getNroRuta() + "/" + year + "\n" +
+                "Fecha: " + fechaCreacionFormatted + "\n" +
+                "Identificador: " + generarAlfanumerico(hojaRuta.getId_hoja_ruta());
+        model.addAttribute("textoQR", textoQR);
+
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        context.setVariables(model.asMap());
+
+        String htmlContent = templateEngine.process("seguimiento/hojaDeRuta", context);
+
+        return ResponseEntity.ok().body(htmlContent);
+    }
+
+    public static String generarAlfanumerico(Long valor) {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder resultado = new StringBuilder();
+        Random random = new Random(valor); // Usamos el Long como semilla para el generador de números aleatorios
+
+        // Determinamos la longitud de la cadena basándonos en el valor de Long (por
+        // ejemplo, 8 caracteres)
+        int longitud = 8;
+
+        for (int i = 0; i < longitud; i++) {
+            int index = random.nextInt(caracteres.length());
+            resultado.append(caracteres.charAt(index));
+        }
+
+        return resultado.toString();
+    }
+
+    @GetMapping("/buscar_documentoInicial/{nroRuta}")
+    public ResponseEntity<?>  buscar_documentoInicial(@PathVariable(name = "nroRuta") int nroRuta, Model model, HttpServletRequest request,
+    HttpServletResponse response) {
+                String gestion = String.valueOf(LocalDate.now().getYear());
+        // List<MovimientoDocumento> flujoDocumentos =
+        // movimientoDocumentoService.obtener_Flujo_Documento(nroRuta);
+        Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+            Usuario user = usuarioService.findById(usuario.getId_usuario());
+            Unidad unidad = user.getUnidad();
+        HojaRuta hojaRuta = hojaRutaService.obtenerHojaRutaPorGestionUnidad(nroRuta, unidad.getId_unidad().intValue(), gestion);
+        String fechaCreacionStr = hojaRuta.getFechaCreacion().toString(); // Supongamos que es una cadena como
+                                                                          // '2024-08-21 16:55:29.393'
+
+        // Usa LocalDateTime y ajusta el formato del DateTimeFormatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        LocalDateTime fechaCreacion = LocalDateTime.parse(fechaCreacionStr, formatter);
+
+        // Formatea la fecha y hora al formato deseado
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm ");
+        String fechaCreacionFormatted = fechaCreacion.format(outputFormatter);
+
+        int year = fechaCreacion.getYear();
+
+        model.addAttribute("hojaRuta", hojaRuta);
+        if (hojaRuta.getTipo_derivacion().equals("Interno")) {
+            Integer id_usuario_emisor = hojaRuta.getUsuario_emisor();
+            Long id_usuario_emisor_long = id_usuario_emisor.longValue();
+            Usuario usuario_emisor = usuarioService.findById(id_usuario_emisor_long);
+            model.addAttribute("usuario_emisor", usuario_emisor);
+            model.addAttribute("interno", true);
+        }
+        if (hojaRuta.getTipo_derivacion().equals("Externo")) {
+            model.addAttribute("usuario_emisor", hojaRuta.getEntidadExterna().getNombre());
+            model.addAttribute("externo", true);
+        }
+
+        String textoQR = "Hoja de Ruta: " + hojaRuta.getNroRuta() + "/" + year + "\n" +
+                "Fecha: " + fechaCreacionFormatted + "\n" +
+                "Identificador: " + generarAlfanumerico(hojaRuta.getId_hoja_ruta());
+        model.addAttribute("textoQR", textoQR);
+
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        context.setVariables(model.asMap());
+
+        String htmlContent = templateEngine.process("seguimiento/hojaDeRuta", context);
+
+        return ResponseEntity.ok().body(htmlContent);
+    }
 }
